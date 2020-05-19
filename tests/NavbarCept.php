15d821074debb9ec2fd6621c95a5a326cea3819e
Oscar Keur <o.e.keur@gmail.com>
2020-05-19 14:13:38 +0200

    Add test for navbar text color

--- tests/NavbarCept.php
+++ tests/NavbarCept.php
@@ -1,9 +1,11 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('check home page has a white header');
+$I->wantTo('check navbar has right design');
 
 $I->amOnPage('/');
 
 $headerBackgroundColor = $I->getCSSValue('nav#header', 'background-color');
+$headerTextColor = $I->getCSSValue('nav#header .top-navigation a.nav-link', 'color');
 
 $I->assertEquals('rgba(255, 255, 255, 1)', $headerBackgroundColor);
+$I->assertEquals('rgba(92, 105, 120, 1)', $headerTextColor);
