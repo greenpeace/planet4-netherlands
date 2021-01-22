cd313b8c646b4095ee29b4aa8db1e5a582941969
Dirk Faber <d.j.faber@outlook.com>
2021-01-22 17:10:44 +0100

    remove navbar test

--- tests/NavbarCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('check navbar has right design');
-
-$I->amOnPage('/');
-
-$headerBackgroundColor = $I->getCSSValue('nav#header', 'background-color');
-
-$I->assertEquals('rgba(255, 255, 255, 1)', $headerBackgroundColor);
