19574c9384303e9813f61463393397253c86e898
Oscar Keur <o.e.keur@gmail.com>
2021-02-11 16:03:19 +0100

    feat: implement newspage acceptance test

--- tests/NewsPageCept.php
+++ tests/NewsPageCept.php
@@ -3,9 +3,8 @@ $I = new AcceptanceTester($scenario);
 $I->amGoingTo('check news pagination works');
 
 $I->amOnPage('/nieuws');
-$I->wait(10);
-$I->waitForElement('.pagination li.page-item:nth-child(4) > a', 5);
-$I->scrollTo(['css' => 'footer '], 20, 50);
+$I->scrollTo(['css' => '.pagination li.page-item:nth-child(4) > a'], 20, 50);
+$I->wait(1);
 $I->click('.pagination li.page-item:nth-child(4) > a');
 $I->SeeInCurrentUrl('/nieuws/2/');
 $I->see('Resultaten - pagina 2');
